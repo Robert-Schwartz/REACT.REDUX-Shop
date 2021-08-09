7b7b739f3d9a39adf0007ce8de4c60383a40798e
Robert Schwartz <bschwartz23@gmail.com>
2021-08-09 10:40:33 -0700

    Implement Adding Items to the Cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 
 function ProductItem(item) {
   const {
@@ -11,22 +13,30 @@ function ProductItem(item) {
     quantity
   } = item;
 
+  const [state, dispatch] = useStoreContext();
+
+	const addToCart = () => {
+		dispatch({
+			type: ADD_TO_CART,
+			product: { ...item, purchaseQuantity: 1 },
+		});
+	};
+
   return (
-    <div className="card px-1 py-1">
-      <Link to={`/products/${_id}`}>
-        <img
-          alt={name}
-          src={`/images/${image}`}
-        />
-        <p>{name}</p>
-      </Link>
-      <div>
-        <div>{quantity} {pluralize("item", quantity)} in stock</div>
-        <span>${price}</span>
-      </div>
-      <button>Add to cart</button>
-    </div>
-  );
+		<div className="card px-1 py-1">
+			<Link to={`/products/${_id}`}>
+				<img alt={name} src={`/images/${image}`} />
+				<p>{name}</p>
+			</Link>
+			<div>
+				<div>
+					{quantity} {pluralize("item", quantity)} in stock
+				</div>
+				<span>${price}</span>
+			</div>
+			<button onClick={addToCart}>Add to cart</button>
+		</div>
+	);
 }
 
 export default ProductItem;
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,10 +5,16 @@ import Cart from "../components/Cart";
 
 // global state needs
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from "../assets/spinner.gif";
+
+import {
+	REMOVE_FROM_CART,
+	UPDATE_CART_QUANTITY,
+	ADD_TO_CART,
+	UPDATE_PRODUCTS,
+} from "../utils/actions";
 
 function Detail() {
 	const [state, dispatch] = useStoreContext();
@@ -19,6 +25,13 @@ function Detail() {
 	const { loading, data } = useQuery(QUERY_PRODUCTS);
 
 	const { products } = state;
+
+	const addToCart = () => {
+		dispatch({
+			type: ADD_TO_CART,
+			product: { ...currentProduct, purchaseQuantity: 1 },
+		});
+	};
 
 	useEffect(() => {
 		if (products.length) {
@@ -43,7 +56,7 @@ function Detail() {
 
 					<p>
 						<strong>Price:</strong>${currentProduct.price}{" "}
-						<button>Add to Cart</button>
+						<button onClick={addToCart}>Add to cart</button>
 						<button>Remove from Cart</button>
 					</p>
 
