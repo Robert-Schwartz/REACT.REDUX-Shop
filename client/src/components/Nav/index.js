19c5791fc8006f8158938e192c51a0bd401e0e03
Robert Schwartz <bschwartz23@gmail.com>
2021-08-09 11:11:51 -0700

    Implement Updating and Deleting Items

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -44,7 +44,7 @@ function Nav() {
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+        -RS Shop
         </Link>
       </h1>
 
